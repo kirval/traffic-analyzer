94b903fcbe437609b9d4c03e673be33e07e5aa0b
Kirill Valyushko <valyushko.kirill@gmail.com>
2018-05-12 12:33:00 +0300

    traffic-analyzer ver 0.1

---
+++ src/Application.java
@@ -0,0 +1,24 @@
+import jxl.write.WriteException;
+import org.opencv.core.Core;
+import tw.edu.sju.ee.commons.nativeutils.NativeUtils;
+
+import java.io.IOException;
+
+public class Application {
+    static {
+        try {
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // used for tests. This library in classpath only
+        } catch (UnsatisfiedLinkError e) {
+            try {
+                NativeUtils.loadLibraryFromJar("opencv_java310"); // during runtime. .DLL within .JAR
+            } catch (IOException e1) {
+                throw new RuntimeException(e1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, WriteException, InterruptedException {
+        GUI gui = new GUI();
+        gui.init();
+    }
+}
